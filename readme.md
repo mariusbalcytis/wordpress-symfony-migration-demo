0fede9959303f413f87f1d188547be950a3d66f8
Marius Balčytis <marius.balcytis@gmail.com>
2018-01-31 00:19:40 +0200

    Introduce unit tests

--- readme.md
+++ readme.md
@@ -2,6 +2,13 @@
 
 ```
 docker-compose up
+composer install
 open http://localhost:8090
 ```
 
+# Testing
+
+```
+composer install
+vendor/bin/phpunit tests
+```
---
+++ tests/WeatherProviderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Maba\Application\Tests;
+
+use Maba\Application\WeatherProvider;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Th3Mouk\YahooWeatherAPI\YahooWeatherAPIInterface;
+
+class WeatherProviderTest extends TestCase
+{
+    public function testGetCurrentWeather()
+    {
+        /** @var MockObject|YahooWeatherAPIInterface $weatherApi */
+        $weatherApi = $this->createMock(YahooWeatherAPIInterface::class);
+        $weatherApi
+            ->expects($this->once())
+            ->method('callApiCityName')
+            ->with('Vilnius')
+            ->will($this->returnValue(
+                ['item' => ['condition' => ['text' => 'Cloudy', 'temp' => 13]]]
+            ));
+
+        $provider = new WeatherProvider($weatherApi);
+        $this->assertSame('Cloudy (13 C)', $provider->getCurrentWeather());
+    }
+}
